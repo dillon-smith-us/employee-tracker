a440fc829b16d03181989bd722a8cae8cdf576d4
Dillon Smith <dillsmth@gmail.com>
2021-07-10 18:50:37 -0500

    removing password

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Drmario1$',
+    password: '',
     database: 'employee_trackerdb',
 });
 
